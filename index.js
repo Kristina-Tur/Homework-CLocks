e085b52bc2f7e7818dafcf2c2ecfec0c83e640fd
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-02-26 20:21:41 +0300

    hw

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 let timerId; // переменная, которая будет хранить ID таймера
+let isRanning = false;
 
+function startClock() {
+  const startButton = document.getElementById('start');
+  startButton.addEventListener('click', function () {
+    if (!isRanning) {
+      timerId = setInterval(updateClock, 1000);
+    }
+  });
+}
 
-const startButton = document.getElementById('start');
-startButton.addEventListener('click', function () {
-  // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ setInterval
-  timerId = setInterval(updateClock, 1000); // запускаем  updateClock() каждую секунду
-});
+function stopClock() {
+  const stopButton = document.getElementById('stop');
+  stopButton.addEventListener('click', function () {
+    isRanning = false;
+    clearInterval(timerId); // останавливаем таймер
+  });
+}
 
-
-const stopButton = document.getElementById('stop');
-stopButton.addEventListener('click', function () {
-  // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ clearInterval
-  clearInterval(timerId); // останавливаем таймер
-});
 
 
 function updateClock() {
   const clock = document.getElementById('clock');
   // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ new Date()
-  const now = new Data();
+  const now = new Date();
   const hours = now.getHours()
   const minutes = now.getMinutes()
   const seconds = now.getSeconds()
   clock.textContent = hours + ':' + minutes + ':' + seconds;
 }
+
+updateClock();
+startClock();
+stopClock()
